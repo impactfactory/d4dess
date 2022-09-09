c99ffbc69a84867311e8a3000fdf380127544871
Kai <webmaster@impactfactory.ch>
2022-09-09 21:36:46 +0200

    Navi, team, colors, primary color, animation

--- themes/if-bt/assets/css-js/index.js
+++ themes/if-bt/assets/css-js/index.js
@@ -324,7 +324,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "55678" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "57301" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
